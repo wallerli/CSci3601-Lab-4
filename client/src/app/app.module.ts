999ad2b35e0db0e448d3852d27aeca69fcc009ae
K.K. Lamberty <lamberty@morris.umn.edu>
2019-10-01 15:37:50 -0500

    Initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {UserComponent} from './users/user.component';
+import {UserListComponent} from './users/user-list.component';
+import {UserListService} from './users/user-list.service';
+import {Routing} from './app.routes';
+import {APP_BASE_HREF} from '@angular/common';
+
+import {CustomModule} from './custom.module';
+import {AddUserComponent} from './users/add-user.component';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    Routing,
+    CustomModule,
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UserListComponent,
+    UserComponent,
+    AddUserComponent
+  ],
+  providers: [
+    UserListService,
+    {provide: APP_BASE_HREF, useValue: '/'},
+  ],
+  entryComponents: [
+    AddUserComponent,
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
